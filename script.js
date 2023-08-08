4058c175fe1c41f2181ad3dc830cf970aee658b7
Prasad Mullamuri <119662453+prasadmull@users.noreply.github.com>
2023-08-09 00:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if(num<=2){
+		return num-1;
+	}
+	let a=0;
+	let b=1;
+	for(let i=2;i<n;i++){
+		let tempt=a+b;
+		a=b;
+		b=tempt;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
